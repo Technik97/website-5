1636672e508e579aec4743e3fe22923691fd4d1c
Technik97 <aniket.97.mane@gmail.com>
2020-11-20 15:35:26 +0530

    Added herosection and styled it..and added pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact />
+          <Route path='/' exact component={Home} />
           <Route path='/services' exact />
           <Route path='/products' exact />
           <Route path='/sign-up' exact />
